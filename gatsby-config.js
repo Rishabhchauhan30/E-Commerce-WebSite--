cf5277f6adea5fb12f43ebeee35425a186eefccb
Rishabh chauhan <64160163+rishabh1057@users.noreply.github.com>
2020-11-13 22:36:52 +0530

    plugin manifest

--- gatsby-config.js
+++ gatsby-config.js
@@ -28,7 +28,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+       // icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
 
     },
@@ -46,7 +46,8 @@ module.exports = {
         apiKey: process.env.SNIPCART_KEY,
         autopop: true
 			}
-		}
+    }
+    
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
